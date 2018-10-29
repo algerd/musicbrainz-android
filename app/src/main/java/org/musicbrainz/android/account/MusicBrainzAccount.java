886b0898c2226e4ba006e2a40c837ba7fc91c07b
Alex <algerd75@mail.ru>
2018-10-29 10:27:49 +0300

    fix

--- app/src/main/java/org/musicbrainz/android/account/MusicBrainzAccount.java
+++ app/src/main/java/org/musicbrainz/android/account/MusicBrainzAccount.java
@@ -5,7 +5,7 @@ import android.os.Parcel;
 
 public class MusicBrainzAccount extends Account {
 
-    public static final String ACCOUNT_TYPE = "ru.javafx.musicbrain";
+    public static final String ACCOUNT_TYPE = "org.musicbrainz.android";
     public static final String ACCESS_TOKEN = "access_token";
     public static final String REFRESH_TOKEN = "refresh_token";
     public static final String EXPIRE_IN = "expire_in";
--- app/src/main/java/org/musicbrainz/android/activity/MainActivity.java
+++ app/src/main/java/org/musicbrainz/android/activity/MainActivity.java
@@ -1,15 +1,20 @@
 package org.musicbrainz.android.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import org.musicbrainz.android.R;
 import org.musicbrainz.android.fragment.SearchFragment;
 import org.musicbrainz.android.fragment.UserSearchFragment;
 import org.musicbrainz.android.intent.ActivityFactory;
 
+import static org.musicbrainz.android.MusicBrainzApp.oauth;
+
 public class MainActivity extends BaseActivity implements
         SearchFragment.SearchFragmentListener,
         UserSearchFragment.FragmentListener {
+
+    private View logInBtn;
 
     @Override
     protected int initContentLayout() {
@@ -19,6 +24,20 @@ public class MainActivity extends BaseActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        logInBtn = findViewById(R.id.log_in_btn);
+        if (!oauth.hasAccount()) {
+            logInBtn.setVisibility(View.VISIBLE);
+            logInBtn.setOnClickListener(v -> ActivityFactory.startLoginActivity(this));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (oauth.hasAccount()) {
+            logInBtn.setVisibility(View.GONE);
+        }
     }
 
     @Override
